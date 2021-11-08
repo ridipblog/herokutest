fccbf2a7ebc53fe6d172bc9c60894f345ac755e4
ridipblog <memorytemp5@gmail.com>
2021-11-07 17:56:09 -0800

    init git

---
+++ index.js
@@ -0,0 +1,18 @@
+const express=require('express');
+const app=express();
+const port=process.env.PORT || 3000;
+app.get("/",(req,res,next)=>{
+	res.send("Home Page Welcome from ridip for laptop");
+});
+app.get("/about",(req,res,next)=>{
+	res.send("About Page Welcome from ridip");
+});
+app.get("/contact",(req,res,next)=>{
+	res.send("Contact Page Welcome from ridip");
+});
+app.get("*",(req,res,next)=>{
+	res.send("Error Page");
+});
+app.listen(port,()=>{
+	console.log("Server runing");
+});
